485ad603d5c07f7f08ed117a0d3f0e06eb7aba60
Dries De Peuter <dries@nousefreak.be>
2014-07-31 21:08:16 +0200

    Add guides

---
+++ src/DOH/GuideBundle/Form/Type/ParameterType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DOH\GuideBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ParameterType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'doh_guide_parameter';
+    }
+}
